e9cbebe024b88228cfbc7393115b9606dfd23471
chandu <chandrasr.palle@gmail.com>
2018-06-18 23:58:11 +0200

    serach form added for movies

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const request = require('request');
 const express = require('express');
 const app = express();
 app.set("view engine", "ejs");
+app.get("/", function(req, res){
+    res.render("search");
+});
 app.get("/results",function(req, res){
-    request("http://www.omdbapi.com/?apikey=thewdb&s=star",function(reqst, response, body){
+
+    const movieName = req.query.searchName;
+    request("http://www.omdbapi.com/?apikey=thewdb&s="+movieName,function(reqst, response, body){
         if(!reqst && response.statusCode == 200){
             const parsedData = JSON.parse(body);
             res.render("results", {data : parsedData});
