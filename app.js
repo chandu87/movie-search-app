7a06f3852e1fc29fe4fedfb718f4352e29f7ea91
chandu <chandrasr.palle@gmail.com>
2018-06-18 23:26:56 +0200

    route added to display results page

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const request = require('request');
 const express = require('express');
 const app = express();
-
+app.set("view engine", "ejs");
 app.get("/results",function(req, res){
     request("http://www.omdbapi.com/?apikey=thewdb&s=star",function(reqst, response, body){
         if(!reqst && response.statusCode == 200){
             const parsedData = JSON.parse(body);
-            res.send(parsedData.Search[0].Title);
+            res.render("results", {data : parsedData});
         }
     });
 
