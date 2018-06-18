4e83b235aca46139391bc05f28976587d7df6f4c
chandu <chandrasr.palle@gmail.com>
2018-06-18 23:19:44 +0200

    omdb api request added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Movie app");
+const request = require('request');
+const express = require('express');
+const app = express();
+
+app.get("/results",function(req, res){
+    request("http://www.omdbapi.com/?apikey=thewdb&s=star",function(reqst, response, body){
+        if(!reqst && response.statusCode == 200){
+            const parsedData = JSON.parse(body);
+            res.send(parsedData.Search[0].Title);
+        }
+    });
+
+});
+app.listen(3000, function(){
+    console.log("Server started at 3000");
+});
+
+
